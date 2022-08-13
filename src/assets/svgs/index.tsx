e78b09f294964f16654b32a57ea006d77e504a15
Nicolas Oliveira <nicksouza861@gmail.com>
2022-08-13 02:57:25 -0300

    feat: project setup

---
+++ src/assets/svgs/index.tsx
@@ -0,0 +1,9 @@
+import { BellIcon } from './icons/bell';
+import { ChatIcon } from './icons/chat';
+import { SearchIcon } from './icons/search';
+
+export const Icons = {
+  chat: <ChatIcon />,
+  bell: <BellIcon />,
+  search: <SearchIcon />,
+};
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,13 +1,17 @@
-import { globalCss } from 'styles/stitches.config';
+import { globalCss } from '../../stitches.config';
 
 const GlobalStyles = globalCss({
   '*': {
     margin: 0,
     padding: 0,
     boxSizing: 'border-box',
-    fontFamily: 'Poppins, sans-serif',
+    fontFamily:
+      '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", Helvetica, "ヒラギノ角ゴ Pro W3", "Hiragino Kaku Gothic Pro", メイリオ, Meiryo, "ＭＳ Ｐゴシック", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
     fontSize: '16px',
+    lineHeight: 'normal',
+    letterSpacing: 'normal',
     color: '#000000',
+    textAlign: 'center',
   },
 
   button: {
@@ -17,7 +21,7 @@ const GlobalStyles = globalCss({
   },
 
   body: {
-    background: '$white',
+    background: '$bg',
   },
 });
 
